5528c2d08d5c2053502df98f03c14c512f674d0c
Bebe Jane <bjornthief@gmail.com>
2022-11-06 20:23:27 +0100

    first

--- src/entrypoints/ConfigScreen.tsx
+++ src/entrypoints/ConfigScreen.tsx
@@ -1,6 +1,5 @@
 import { RenderConfigScreenCtx } from 'datocms-plugin-sdk';
-import { Canvas, ContextInspector } from 'datocms-react-ui';
-import s from './styles.module.css';
+import { Canvas } from 'datocms-react-ui';
 
 type Props = {
   ctx: RenderConfigScreenCtx;
@@ -9,10 +8,7 @@ type Props = {
 export default function ConfigScreen({ ctx }: Props) {
   return (
     <Canvas ctx={ctx}>
-      <p>Welcome to your plugin! This is your config screen!</p>
-      <div className={s.inspector}>
-        <ContextInspector />
-      </div>
+      <p>Konstnärscentrum plugin</p>
     </Canvas>
   );
 }

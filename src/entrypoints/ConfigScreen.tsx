8db0d8c6ce9ba230c195e03e4f1fa7542dac9d6d
Bebe Jane <bjornthief@gmail.com>
2022-12-20 15:56:12 +0100

    basic auth

--- src/entrypoints/ConfigScreen.tsx
+++ src/entrypoints/ConfigScreen.tsx
@@ -1,5 +1,5 @@
 import { RenderConfigScreenCtx } from 'datocms-plugin-sdk';
-import { Canvas, Button } from 'datocms-react-ui';
+import { Canvas, Button, TextField } from 'datocms-react-ui';
 import { useState } from 'react';
 import ModelSelector from './ModelSelector';
 
@@ -8,20 +8,24 @@ export type Props = {
 };
 
 type Parameters = {
-  helpModels: string
+  helpModels: string,
+  basicAuthUsername: string,
+  basicAuthPassword: string,
 }
 
 export default function ConfigScreen({ ctx }: Props) {
 
   const parameters = ctx.plugin.attributes.parameters as Parameters;
   const [helpModels, setHelpModels] = useState<string | undefined>()
+  const [basicAuthPassword, setBasicAuthPassword] = useState<string | undefined>();
+  const [basicAuthUsername, setBasicAuthUsername] = useState<string | undefined>();
 
   const saveSettings: React.MouseEventHandler = (e) => {
     e.preventDefault()
-    ctx.updatePluginParameters({ ...parameters, helpModels });
+    ctx.updatePluginParameters({ ...parameters, helpModels, basicAuthPassword, basicAuthUsername });
   }
 
-  const hasChanged = JSON.stringify({ ...parameters, helpModels }) !== JSON.stringify(parameters)
+  const hasChanged = JSON.stringify({ ...parameters, helpModels, basicAuthPassword, basicAuthUsername }) !== JSON.stringify(parameters)
   const currentHelpModels = ctx.plugin.attributes.parameters.helpModels ? JSON.parse(ctx.plugin.attributes.parameters.helpModels as string) : []
 
   return (
@@ -33,6 +37,22 @@ export default function ConfigScreen({ ctx }: Props) {
           currentValue={currentHelpModels}
           isMulti={true}
           onChange={(options) => { setHelpModels(JSON.stringify(options)) }}
+        />
+      </p>
+      <p>
+        <TextField
+          id="basicAuthUsername"
+          label="Username"
+          name="basicAuthUsername"
+          value={basicAuthUsername}
+          onChange={(val) => setBasicAuthUsername(val as string)}
+        />
+        <TextField
+          id="basicAuthPassword"
+          label="Password"
+          name="basicAuthPassword"
+          value={basicAuthPassword}
+          onChange={(val) => setBasicAuthPassword(val as string)}
         />
       </p>
       <br />

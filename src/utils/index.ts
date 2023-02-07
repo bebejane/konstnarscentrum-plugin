37f7a75a6d466aea388fe3dbf02b7afc8d1180af
Bebe Jane <bjornthief@gmail.com>
2023-02-07 16:24:51 +0100

    1

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,2 @@
 export const isDev = document.location.hostname === 'localhost';
-export const siteUrl = isDev ? 'http://localhost:3001' : 'https://beta.konstnarscentrum.org'
+export const siteUrl = isDev ? 'http://localhost:3001' : 'https://www.konstnarscentrum.org'
